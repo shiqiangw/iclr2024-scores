899760069e0474eac245afc4292ff2c8ac595b18
Shiqiang Wang <wangshiq@us.ibm.com>
2023-11-10 13:52:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# iclr2024-scores
+# ICLR 2024 Scores from OpenReview
+
+A simple script to extract scores of publicly available ICLR submissions from OpenReview.
+
+Prerequisite: Install the OpenReview Python client following Step 1 in https://docs.openreview.net/getting-started/using-the-api/installing-and-instantiating-the-python-client
+
+Run `pip install -r requirements.txt` for insatlling basic requirements
+
+Run `python3 main.py` to get a spreadsheet including paper titles and scores, in CSV format. The result is saved in `output.csv`.
+
+Update: `output.csv` is sorted according to average score
+
+Note: Withdrawn papers are excluded from the output.
+
+**Recommendation:** Let's not overload the OpenReview server, so don't run this too often :)
---
+++ main.py
@@ -0,0 +1,40 @@
+import openreview
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import pickle
+
+client = openreview.api.OpenReviewClient(baseurl='https://api2.openreview.net')
+
+submissions = client.get_all_notes(invitation='ICLR.cc/2024/Conference/-/Submission', details='directReplies')
+
+reviews = []
+for submission in submissions:
+    reviews = reviews + [reply for reply in submission.details['directReplies'] if reply['invitations'][0].endswith('Official_Review')]
+
+scores_dict = {}
+for r in reviews:
+    if r['forum'] not in scores_dict:
+        scores_dict[r['forum']] = []
+    scores_dict[r['forum']].append(int(r['content']['rating']['value'].split(':')[0]))
+
+statistics = []
+all_data = []
+for s in tqdm(submissions):
+    if s.forum not in scores_dict:
+        continue
+    title = s.content['title']['value']
+    if 'primary_area' in s.content:
+        area = s.content['primary_area']['value']
+    else:
+        area = ''
+    scores = scores_dict[s.forum]
+    avg_score = np.mean(scores)
+    std_score = np.std(scores)
+    all_data.append([ title, str(avg_score), str(std_score), ';'.join([str(i) for i in scores]), area])
+
+
+df = pd.DataFrame(all_data, columns=['Title', 'Average Score', 'Standard Deviation', 'Individual Scores', 'Author-defined Area'])
+df = df.sort_values(by=['Average Score'], ascending=False, ignore_index=True)
+df.index = np.arange(1, len(df)+1)
+df.to_csv('output.csv', index='True')
